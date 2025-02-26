e42bb84e37afc3907cd7f7f3f525e6d913ee152e
PrabhuSubbiah <81504636+PrabhuSubbiah@users.noreply.github.com>
2025-02-26 03:36:56 -0500

    Added missing scripts files
    Script files have been added

---
+++ BeSpokedApp/BeSpokedApp/Utilities/UtilityClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Web.Mvc;
+using System.Web.ModelBinding;
+
+namespace BeSpokedApp.Utilities
+{
+    public static class UtilityClass
+    {
+            public static string GetAPIUri()
+            {
+                string url = ConfigurationManager.AppSettings.Get("BESpokedURI");
+
+                return url;
+            }
+            public static HttpClient GetHTTPClient()
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(GetAPIUri());
+                //            client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6,ru;q=0.4");
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+                ServicePointManager.ServerCertificateValidationCallback = new
+                RemoteCertificateValidationCallback
+                (
+                    delegate { return true; }
+                );
+                return client;
+            }
+        public static IEnumerable<T> GetLookUps<T>(T typeOfLookup)
+        {
+            IEnumerable<T> lookups = null;
+
+            using (var client = UtilityClass.GetHTTPClient())
+            {
+                var responseTask = client.GetAsync(string.Format(@"api/BeSpokeBus/" + GetFunctionNameForLookup(typeOfLookup)));
+                responseTask.Wait();
+
+                var result = (HttpResponseMessage)responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<T>>();
+                    readTask.Wait();
+
+                    lookups =(IEnumerable<T>) readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    if (result.StatusCode == HttpStatusCode.NotAcceptable)
+                    {
+                        string outStr = result.Content.ReadAsStringAsync().Result;
+                    }
+                    else if (result.StatusCode == HttpStatusCode.InternalServerError)
+                    {
+                        string outStr = result.Content.ReadAsStringAsync().Result;
+                        Exception ex = new Exception(outStr);
+                        throw ex;
+                    }
+                    else if (result.StatusCode == HttpStatusCode.ExpectationFailed)
+                    {
+                        Exception ex = result.Content.ReadAsStringAsync().Exception;
+                        throw ex;
+                    }
+                }
+            }
+            return lookups;
+        }
+        private static string GetFunctionNameForLookup<T>(T typeName)
+        {
+            switch(typeName.GetType().Name)
+            {
+                case "StyleLookUp" :
+                    return "GetStyleLookUps";
+                case "ManagerLookUp":
+                    return "GetManagerLookUps";
+                case "CustomerLookUp":
+                    return "GetCustomerLookUps";
+                case "SalesPersonLookUp":
+                    return "GetSalesPersonLookUps";
+                case "ProductLookUp":
+                    return "GetProductLookUps";
+                default:
+                    return "GetStyleLookUps";
+            }
+
+        }
+    }
+}
---
+++ BeSpokedApp/BeSpokedApp/Utilities/VMMapper.cs
@@ -0,0 +1,91 @@
+﻿using BeSpokedApp.Models;
+using BeSpokedDAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeSpokedApp.Utilities
+{
+    public static class VMMapper
+    {
+        public static ProductViewModel MapToProductViewModel(this Product source)
+        {
+            return new ProductViewModel()
+            {
+                id = source.id,
+                manufacturer = source.manufacturer,
+                style_id = source.style_id,
+                purchase_price = source.purchase_price,
+                sales_price = source.sales_price,
+                quantity = source.quantity,
+                commission = source.commission
+            };
+        }
+
+        public static Product MapFromProductViewModel(this ProductViewModel source)
+        {
+            return new Product()
+            {
+                id = source.id,
+                manufacturer = source.manufacturer,
+                style_id = source.style_id,
+                purchase_price = source.purchase_price,
+                sales_price = source.sales_price,
+                quantity = source.quantity,
+                commission = source.commission
+            };
+        }
+
+    public static SalesPersonViewModel MapToSalesPersonViewModel(this SalesPerson source)
+        {
+            return new SalesPersonViewModel()
+            {
+                id = source.id,
+                first_name = source.first_name,
+                last_name = source.last_name,
+                address = source.address,
+                phone = source.phone,
+                start_date = source.start_date,
+                termination_date = source.termination_date,
+                manager_id = source.manager_id,
+            };
+        }
+        public static SalesPerson MapFromSalesPersonViewModel(this SalesPersonViewModel source)
+        {
+            return new SalesPerson()
+            {
+                id = source.id,
+                first_name = source.first_name,
+                last_name = source.last_name,
+                address = source.address,
+                phone = source.phone,
+                start_date = source.start_date,
+                termination_date = source.termination_date,
+                manager_id = source.manager_id
+            };
+        }
+        public static SalesViewModel MapToSalesViewModel(this Sales source)
+        {
+            return new SalesViewModel()
+            {
+                id = source.id,
+                product_id = source.product_id,
+                customer_id = source.customer_id,
+                sales_person_id = source.sales_person_id,
+                sales_date = source.sales_date,
+            };
+        }
+        public static Sales MapFromSalesViewModel(this SalesViewModel source)
+        {
+            return new Sales()
+            {
+                id = source.id,
+                product_id = source.product_id,
+                customer_id = source.customer_id,
+                sales_person_id = source.sales_person_id,
+                sales_date = source.sales_date,
+            };
+        }
+    }
+}
